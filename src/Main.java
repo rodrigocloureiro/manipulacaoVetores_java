5a74893dcbb2376c36bed5639cc32f1e9345aebd
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-09-11 16:40:32 -0300

    exercício 5 feito

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         /* 1- Inversão de Array -> Escreva um programa que recebe um array de 10 números inteiros do usuário e
         os mostra na ordem inversa. */
         System.out.println("1- Inversão de Array");
+
         int[] numeros = new int[10];
 
         System.out.println("Informe 10 números:");
@@ -22,6 +23,7 @@ public class Main {
         /* 2- Menor e Maior Valor -> Escreva um programa que recebe um array de números inteiros do usuário e
         determina o menor e o maior número dentro desse array. Não utilize funções prontas para essa finalidade. */
         System.out.println("\n2- Maior e Menor Valor");
+
         int[] arrNumeros = new int[6];
         int maior = Integer.MIN_VALUE, menor = Integer.MAX_VALUE;
 
@@ -40,6 +42,7 @@ public class Main {
         o programa deve pedir ao usuário um índice (entre 0 e 4). O programa deve então mostrar o nome que está
         na posição correspondente a esse índice. */
         System.out.println("\n3- Nome em Posição");
+
         String[] nomes = new String[5];
         sc.nextLine();
 
@@ -59,7 +62,7 @@ public class Main {
         se torna o segundo, e assim por diante). Mostra o array invertido na tela. */
         System.out.println("\n4- Cópia Invertida");
 
-        System.out.println("Informe 5 palavras ou nomes:");
+        System.out.println("Informe 5 strings:");
         String[] arrStrings = new String[5];
         String[] arrInvertido = new String[5];
         sc.nextLine();
@@ -74,6 +77,23 @@ public class Main {
             System.out.println(arrInvertido[i]);
         }
 
+        /* 5- Contagem de Caracteres -> Peça ao usuário para inserir uma série de palavras (strings).
+        Armazene essas palavras em um array e, em seguida, calcule e mostre quantos caracteres no total
+        foram inseridos, somando todos os caracteres de todas as palavras. */
+        System.out.println("\n5- Contagem de Caracteres");
 
+        String[] arrPalavras = new String[4];
+        int totalCaracteres = 0;
+
+        System.out.println("Informe 4 palavras:");
+        for (int i = 0; i < arrPalavras.length; i++) {
+            arrPalavras[i] = sc.nextLine();
+            totalCaracteres += arrPalavras[i].length();
+        }
+
+        System.out.println("********************");
+        System.out.printf("Foram digitados %d caracteres", totalCaracteres);
+
+//        System.out.println("\n6- Identificando Números Duplicados");
     }
 }
